b6cfc3ff62500d4121a93f61696d0846821a6cec
Christopher-James Santiago <54423958+limitedink@users.noreply.github.com>
2025-11-04 18:10:54 +1100

    waybar: Source API key in weather script to enable functionality

--- waybar/scripts/weather.zsh
+++ waybar/scripts/weather.zsh
@@ -1,9 +1,8 @@
-# ~/.config/waybar/scripts/weather.zsh
 #!/usr/bin/env zsh
+source ~/.zshrc.local
 CITY="Melbourne,AU"
 API_KEY="$OPENWEATHER_API_KEY"
 WEATHER=$(curl -sf "https://api.openweathermap.org/data/2.5/weather?q=$CITY&appid=$API_KEY&units=metric")
 TEMP=$(jq -r '.main.temp' <<< "$WEATHER")
 DESC=$(jq -r '.weather[0].description' <<< "$WEATHER")
 echo "🌤 ${TEMP}°C ${DESC}"
-
